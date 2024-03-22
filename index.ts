59a3057dd46a614a239f4844c962c44ece153d6d
Luke Weetendorf <luke.weetendorf@cloudflight.io>
2024-03-22 08:46:35 +0100

    add package.json

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import * as fs from 'fs';
 import * as path from 'path'
 
+interface Elf {
+    inventory: number[]
+}
+
+/*---------Run code Below----------*/
+
 const input = fs.readFileSync(path.join(__dirname, './in.txt'), {
     encoding: 'utf-8'
 })
-console.log(input)
-
-interface Elf {
-    inventory: number[]
-} 
 
 const elfList: Elf[] = [];
 
