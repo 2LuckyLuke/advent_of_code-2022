b284603198edd07fd78d2ef346b2e293fa0feabc
Luke Weetendorf <luke.weetendorf@protonmail.com>
2024-04-05 13:19:42 +0200

    4-1: working

--- index.ts
+++ index.ts
@@ -1,8 +1,55 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+interface Range {
+    start: number;
+    end: number;
+}
+interface Pair {
+    firstRange: Range;
+    secondRange: Range;
+}
+
+function getPairFromString(pairString: string): Pair {
+    const stringRanges: string[] = pairString.split(',');
+    const firstRangeString = stringRanges[0].split('-');
+    const firstRange: Range = {
+        start: Number(firstRangeString[0]),
+        end: Number(firstRangeString[1])
+    };
+    const secondRangeString = stringRanges[1].split('-');
+    const secondRange: Range = {
+        start: Number(secondRangeString[0]),
+        end: Number(secondRangeString[1])
+    };
+    const pair: Pair = { firstRange, secondRange };
+    return pair;
+}
+
+function isRangeContained(pair: Pair): boolean {
+    if (
+        (pair.firstRange.start <= pair.secondRange.start && pair.firstRange.end >= pair.secondRange.end) ||
+        (pair.secondRange.start <= pair.firstRange.start && pair.secondRange.end >= pair.firstRange.end)
+    ) {
+        return true;
+    }
+    return false;
+}
+
 /*---------Run code Below----------*/
 
 const input = fs.readFileSync(path.join(__dirname, './in.txt'), { encoding: 'utf-8' });
 
-const individualGames = input.split('\r\n');
+const pairStrings = input.split('\n');
+
+const pairs = pairStrings.map((pairString) => getPairFromString(pairString));
+
+let totalContainedPairs = 0;
+
+for (let i = 0; i < pairs.length; i++) {
+    if (isRangeContained(pairs[i])) {
+        totalContainedPairs += 1;
+    }
+}
+
+console.log(totalContainedPairs);
