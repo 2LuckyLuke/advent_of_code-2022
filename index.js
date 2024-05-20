3d3ae061eef6789529d25c44a62ab0f9a3f6c5fb
Luke Weetendorf <luke.weetendorf@protonmail.com>
2024-05-20 17:12:18 +0200

    5-1 working

--- index.js
+++ index.js
@@ -1,106 +1,69 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
-var path = require("path");
-var RpsValues;
-(function (RpsValues) {
-    RpsValues[RpsValues["ROCK"] = 1] = "ROCK";
-    RpsValues[RpsValues["PAPER"] = 2] = "PAPER";
-    RpsValues[RpsValues["SCISSORS"] = 3] = "SCISSORS";
-})(RpsValues || (RpsValues = {}));
-var RspResults;
-(function (RspResults) {
-    RspResults[RspResults["LOSE"] = 0] = "LOSE";
-    RspResults[RspResults["DRAW"] = 3] = "DRAW";
-    RspResults[RspResults["WIN"] = 6] = "WIN";
-})(RspResults || (RspResults = {}));
-function getPlayerMove(opponentValue, gameResult) {
-    switch (true) {
-        case gameResult === RspResults.DRAW:
-            return opponentValue;
-        case opponentValue === RpsValues.ROCK && gameResult === RspResults.WIN:
-            return RpsValues.PAPER;
-        case opponentValue === RpsValues.PAPER && gameResult === RspResults.WIN:
-            return RpsValues.SCISSORS;
-        case opponentValue === RpsValues.SCISSORS && gameResult === RspResults.WIN:
-            return RpsValues.ROCK;
-        case opponentValue === RpsValues.ROCK && gameResult === RspResults.LOSE:
-            return RpsValues.SCISSORS;
-        case opponentValue === RpsValues.PAPER && gameResult === RspResults.LOSE:
-            return RpsValues.ROCK;
-        case opponentValue === RpsValues.SCISSORS && gameResult === RspResults.LOSE:
-            return RpsValues.PAPER;
-        default:
-            console.log('unexpected player move');
-            return RpsValues.ROCK;
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+function parseStacks(stackListString) {
+    const stacks = [];
+    const collumns = stackListString.split('\n');
+    while (collumns[0].length >= 2) {
+        const singleStack = { crates: [] };
+        collumns.forEach((collumn, index) => {
+            const firstLetter = collumn.charAt(1);
+            if (firstLetter === ' ' || Number.isInteger(Number(firstLetter))) {
+                collumns[index] = collumn.slice(4);
+                return;
+            }
+            singleStack.crates.unshift(firstLetter);
+            collumns[index] = collumn.slice(4);
+        });
+        stacks.push(singleStack);
     }
+    return stacks;
 }
-function getGameFromString(stringGame) {
-    var stringValues = stringGame.split(' ');
-    var opponentValue;
-    switch (stringValues[0]) {
-        case 'A':
-            opponentValue = RpsValues.ROCK;
-            break;
-        case 'B':
-            opponentValue = RpsValues.PAPER;
-            break;
-        case 'C':
-            opponentValue = RpsValues.SCISSORS;
-            break;
-        default:
-            console.log('invalid String on 1');
-            opponentValue = RpsValues.ROCK;
-    }
-    var gameResult;
-    switch (stringValues[1]) {
-        case 'X':
-            gameResult = RspResults.LOSE;
-            break;
-        case 'Y':
-            gameResult = RspResults.DRAW;
-            break;
-        case 'Z':
-            gameResult = RspResults.WIN;
-            break;
-        default:
-            console.log('invalid String on 2');
-            gameResult = RspResults.LOSE;
-    }
-    return { opponentValue: opponentValue, playerValue: getPlayerMove(opponentValue, gameResult) };
+function parseInstructions(instructionList) {
+    return instructionList.split('\n').map((stringInstruction) => {
+        const instructionWords = stringInstruction.split(' ');
+        return {
+            amount: Number(instructionWords[1]),
+            start: Number(instructionWords[3]) - 1,
+            goal: Number(instructionWords[5]) - 1
+        };
+    });
 }
-function getGameResult(game) {
-    var opponentValue = game.opponentValue, playerValue = game.playerValue;
-    switch (true) {
-        case opponentValue === playerValue:
-            return RspResults.DRAW;
-        case opponentValue === RpsValues.ROCK && playerValue === RpsValues.PAPER:
-            return RspResults.WIN;
-        case opponentValue === RpsValues.ROCK && playerValue === RpsValues.SCISSORS:
-            return RspResults.LOSE;
-        case opponentValue === RpsValues.PAPER && playerValue === RpsValues.ROCK:
-            return RspResults.LOSE;
-        case opponentValue === RpsValues.PAPER && playerValue === RpsValues.SCISSORS:
-            return RspResults.WIN;
-        case opponentValue === RpsValues.SCISSORS && playerValue === RpsValues.ROCK:
-            return RspResults.WIN;
-        case opponentValue === RpsValues.SCISSORS && playerValue === RpsValues.PAPER:
-            return RspResults.LOSE;
-        default:
-            console.log('unexpected game values');
-            return RspResults.LOSE;
+function executeInstruction(stacks, instruction) {
+    var _a;
+    for (let i = 0; i < instruction.amount; i++) {
+        stacks[instruction.goal].crates.push((_a = stacks[instruction.start].crates.pop()) !== null && _a !== void 0 ? _a : 'no element');
     }
-}
-function getGameScore(game) {
-    var score = 0;
-    score += Number(getGameResult(game));
-    return score + Number(game.playerValue);
+    return stacks;
 }
 /*---------Run code Below----------*/
-var input = fs.readFileSync(path.join(__dirname, './in.txt'), { encoding: 'utf-8' });
-var individualGames = input.split('\r\n');
-var games = [];
-individualGames.forEach(function (stringGame) { return games.push(getGameFromString(stringGame)); });
-var totalScore = 0;
-games.forEach(function (game) { return (totalScore += getGameScore(game)); });
-console.log(totalScore);
+const input = fs.readFileSync(path.join(__dirname, './in.txt'), { encoding: 'utf-8' });
+const inputs = input.split('\n\n');
+let stacks = parseStacks(inputs[0]);
+const instructions = parseInstructions(inputs[1]);
+instructions.forEach((instruction) => (stacks = executeInstruction(stacks, instruction)));
+console.log(stacks);
--- index.ts
+++ index.ts
@@ -1,8 +1,60 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+interface Stack {
+    crates: string[];
+}
+
+interface Instruction {
+    start: number;
+    goal: number;
+    amount: number;
+}
+
+function parseStacks(stackListString: string): Stack[] {
+    const stacks: Stack[] = [];
+    const collumns = stackListString.split('\n');
+    while (collumns[0].length >= 2) {
+        const singleStack: Stack = { crates: [] };
+        collumns.forEach((collumn, index) => {
+            const firstLetter = collumn.charAt(1);
+            if (firstLetter === ' ' || Number.isInteger(Number(firstLetter))) {
+                collumns[index] = collumn.slice(4);
+                return;
+            }
+            singleStack.crates.unshift(firstLetter);
+            collumns[index] = collumn.slice(4);
+        });
+        stacks.push(singleStack);
+    }
+    return stacks;
+}
+
+function parseInstructions(instructionList: string): Instruction[] {
+    return instructionList.split('\n').map((stringInstruction) => {
+        const instructionWords = stringInstruction.split(' ');
+
+        return {
+            amount: Number(instructionWords[1]),
+            start: Number(instructionWords[3]) - 1,
+            goal: Number(instructionWords[5]) - 1
+        };
+    });
+}
+
+function executeInstruction(stacks: Stack[], instruction: Instruction): Stack[] {
+    for (let i = 0; i < instruction.amount; i++) {
+        stacks[instruction.goal].crates.push(stacks[instruction.start].crates.pop() ?? 'no element');
+    }
+    return stacks;
+}
+
 /*---------Run code Below----------*/
 
 const input = fs.readFileSync(path.join(__dirname, './in.txt'), { encoding: 'utf-8' });
 
-const individualGames = input.split('\r\n');
+const inputs = input.split('\n\n');
+let stacks = parseStacks(inputs[0]);
+const instructions = parseInstructions(inputs[1]);
+instructions.forEach((instruction) => (stacks = executeInstruction(stacks, instruction)));
+console.log(stacks);
