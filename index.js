8bef6c3df9619d93c306117af3937c381076ce55
Luke Weetendorf <luke.weetendorf@protonmail.com>
2024-05-26 21:40:07 +0200

    6-1 working

--- index.js
+++ index.js
@@ -1,106 +1,41 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
-var path = require("path");
-var RpsValues;
-(function (RpsValues) {
-    RpsValues[RpsValues["ROCK"] = 1] = "ROCK";
-    RpsValues[RpsValues["PAPER"] = 2] = "PAPER";
-    RpsValues[RpsValues["SCISSORS"] = 3] = "SCISSORS";
-})(RpsValues || (RpsValues = {}));
-var RspResults;
-(function (RspResults) {
-    RspResults[RspResults["LOSE"] = 0] = "LOSE";
-    RspResults[RspResults["DRAW"] = 3] = "DRAW";
-    RspResults[RspResults["WIN"] = 6] = "WIN";
-})(RspResults || (RspResults = {}));
-function getPlayerMove(opponentValue, gameResult) {
-    switch (true) {
-        case gameResult === RspResults.DRAW:
-            return opponentValue;
-        case opponentValue === RpsValues.ROCK && gameResult === RspResults.WIN:
-            return RpsValues.PAPER;
-        case opponentValue === RpsValues.PAPER && gameResult === RspResults.WIN:
-            return RpsValues.SCISSORS;
-        case opponentValue === RpsValues.SCISSORS && gameResult === RspResults.WIN:
-            return RpsValues.ROCK;
-        case opponentValue === RpsValues.ROCK && gameResult === RspResults.LOSE:
-            return RpsValues.SCISSORS;
-        case opponentValue === RpsValues.PAPER && gameResult === RspResults.LOSE:
-            return RpsValues.ROCK;
-        case opponentValue === RpsValues.SCISSORS && gameResult === RspResults.LOSE:
-            return RpsValues.PAPER;
-        default:
-            console.log('unexpected player move');
-            return RpsValues.ROCK;
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+function isSequenceDiffrent(a, b, c, d) {
+    if (a === b || a === c || a === d || b === c || b === d || c === d) {
+        return false;
     }
-}
-function getGameFromString(stringGame) {
-    var stringValues = stringGame.split(' ');
-    var opponentValue;
-    switch (stringValues[0]) {
-        case 'A':
-            opponentValue = RpsValues.ROCK;
-            break;
-        case 'B':
-            opponentValue = RpsValues.PAPER;
-            break;
-        case 'C':
-            opponentValue = RpsValues.SCISSORS;
-            break;
-        default:
-            console.log('invalid String on 1');
-            opponentValue = RpsValues.ROCK;
-    }
-    var gameResult;
-    switch (stringValues[1]) {
-        case 'X':
-            gameResult = RspResults.LOSE;
-            break;
-        case 'Y':
-            gameResult = RspResults.DRAW;
-            break;
-        case 'Z':
-            gameResult = RspResults.WIN;
-            break;
-        default:
-            console.log('invalid String on 2');
-            gameResult = RspResults.LOSE;
-    }
-    return { opponentValue: opponentValue, playerValue: getPlayerMove(opponentValue, gameResult) };
-}
-function getGameResult(game) {
-    var opponentValue = game.opponentValue, playerValue = game.playerValue;
-    switch (true) {
-        case opponentValue === playerValue:
-            return RspResults.DRAW;
-        case opponentValue === RpsValues.ROCK && playerValue === RpsValues.PAPER:
-            return RspResults.WIN;
-        case opponentValue === RpsValues.ROCK && playerValue === RpsValues.SCISSORS:
-            return RspResults.LOSE;
-        case opponentValue === RpsValues.PAPER && playerValue === RpsValues.ROCK:
-            return RspResults.LOSE;
-        case opponentValue === RpsValues.PAPER && playerValue === RpsValues.SCISSORS:
-            return RspResults.WIN;
-        case opponentValue === RpsValues.SCISSORS && playerValue === RpsValues.ROCK:
-            return RspResults.WIN;
-        case opponentValue === RpsValues.SCISSORS && playerValue === RpsValues.PAPER:
-            return RspResults.LOSE;
-        default:
-            console.log('unexpected game values');
-            return RspResults.LOSE;
-    }
-}
-function getGameScore(game) {
-    var score = 0;
-    score += Number(getGameResult(game));
-    return score + Number(game.playerValue);
+    return true;
 }
 /*---------Run code Below----------*/
-var input = fs.readFileSync(path.join(__dirname, './in.txt'), { encoding: 'utf-8' });
-var individualGames = input.split('\r\n');
-var games = [];
-individualGames.forEach(function (stringGame) { return games.push(getGameFromString(stringGame)); });
-var totalScore = 0;
-games.forEach(function (game) { return (totalScore += getGameScore(game)); });
-console.log(totalScore);
+const input = fs.readFileSync(path.join(__dirname, './in.txt'), { encoding: 'utf-8' });
+for (let i = 0; i < input.length - 3; i++) {
+    if (isSequenceDiffrent(input.charAt(i), input.charAt(i + 1), input.charAt(i + 2), input.charAt(i + 3))) {
+        console.log(i + 4);
+        i = input.length;
+    }
+}
--- index.ts
+++ index.ts
@@ -1,8 +1,20 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+function isSequenceDiffrent(a: string, b: string, c: string, d: string): boolean {
+    if (a === b || a === c || a === d || b === c || b === d || c === d) {
+        return false;
+    }
+    return true;
+}
+
 /*---------Run code Below----------*/
 
 const input = fs.readFileSync(path.join(__dirname, './in.txt'), { encoding: 'utf-8' });
 
-const individualGames = input.split('\r\n');
+for (let i = 0; i < input.length - 3; i++) {
+    if (isSequenceDiffrent(input.charAt(i), input.charAt(i + 1), input.charAt(i + 2), input.charAt(i + 3))) {
+        console.log(i + 4);
+        i = input.length;
+    }
+}
